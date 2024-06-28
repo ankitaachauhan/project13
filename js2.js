11055a900bc025ae5c8486fdc6dc953db1233fdf
Ankita chauhan <ankitach9616@gmain.com>
2024-06-27 15:04:11 -0900

    13th version

---
+++ js2.js
@@ -0,0 +1,42 @@
+function f1()
+{
+    var a=document.getElementsByTagName("p");
+    for(var r=0;r<=a.length;r++)
+        a[r].style.color="pink";
+}
+function f2()
+{
+    var b=document.getElementsByTagName("p");
+    for(var r=0;r<=b.length;r++)
+        {
+            b[r].style.color="red";
+        }
+}
+function f3()
+{
+    var a=document.getElementsByTagName("p");
+    for(var r=0;r<=a.length;r++)
+        {
+            a[r].style.color="yellow";
+        }
+    
+} 
+function f4()
+{
+    var a=document.getElementsByTagName("p");
+    for(var r=0;r<=a.length;r++)
+        {
+            a[r].style.color="blue";
+        }
+    
+
+}
+function f5(c)
+{
+    var a=document.getElementsByTagName("p");
+    for(var r=0;r<=a.length;r++)
+        {
+            a[r].style.color=c;
+        }
+
+}
